30783c56e237f0144ba56540ae03d99f2ad397f9
vasilisdimitriou <vasilisdmr@gmail.com>
2020-01-02 21:18:21 +0200

    added CsvReader class with ReadFirstNCountries method.

---
+++ TopTenCountries/CsvReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TopTenCountries
+{
+    public class CsvReader
+    {
+        public CsvReader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        private string _csvFilePath;
+
+        public Country ReadFirstNCoutries(int nCountries)
+        {
+            return null;
+        }
+    }
+}
