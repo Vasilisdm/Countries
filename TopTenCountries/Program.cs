0dbef0aeb635bc201acb99acc50a110dd3d78f9f
vasilisdmr <vasilisdmr@gmail.com>
2020-01-04 16:37:57 +0200

    Defined a fictionary country and inserted it before the country with population 2mil.

--- TopTenCountries/Program.cs
+++ TopTenCountries/Program.cs
@@ -13,6 +13,10 @@ namespace CountriesPopulation
 
             List<Country> countries = csvReader.ReadAllCountries();
 
+            Country lilliput = new Country("Lilliput", "LIL", "SomeWhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(c=>c.Population < 2_000_000);
+            countries.Insert(lilliputIndex, lilliput);
+
             foreach (Country country in countries)
             {
                 Console.WriteLine($"Population: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}, Country Name: {country.Name}");
